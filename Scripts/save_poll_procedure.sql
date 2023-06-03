490a665e87891eef32032a4203e48197dc6588b1
JS Martinez <jason.guerra253@gmail.com>
2023-06-03 12:38:16 -0600

    fixed order save questions and options

--- Scripts/save_poll_procedure.sql
+++ Scripts/save_poll_procedure.sql
@@ -6,6 +6,8 @@ BEGIN
     DECLARE json_poll_questions_options JSON DEFAULT JSON_ARRAY();
     DECLARE len_json_poll_questions INT DEFAULT 0;
     DECLARE len_json_poll_questions_options INT DEFAULT 0;
+    DECLARE count_questions INT DEFAULT 0;
+    DECLARE count_questions_options INT DEFAULT 0;
 
     set json_poll_general = JSON_EXTRACT(json_poll, '$.general');
     set json_poll_questions = JSON_EXTRACT(json_poll, '$.questions');
@@ -35,7 +37,7 @@ BEGIN
                  JSON_VALUE(json_poll_general, '$.user_poll_id')
              );
     set @id_poll = LAST_INSERT_ID();
-    while len_json_poll_questions > 0 do
+    while count_questions < len_json_poll_questions do
             INSERT INTO encuestas_questions(
                 title_question,
                 type_question_question_id,
@@ -44,14 +46,14 @@ BEGIN
                         JSON_VALUE(
                                 JSON_EXTRACT(
                                         json_poll_questions,
-                                        CONCAT('$[', len_json_poll_questions-1, ']')
+                                        CONCAT('$[', count_questions, ']')
                                     ),
                                 '$.title_question'
                             ),
                         JSON_VALUE(
                                 JSON_EXTRACT(
                                         json_poll_questions,
-                                        CONCAT('$[', len_json_poll_questions-1, ']')
+                                        CONCAT('$[', count_questions, ']')
                                     ),
                                 '$.type_question'
                             ),
@@ -61,12 +63,12 @@ BEGIN
 
             set json_poll_questions_options = JSON_EXTRACT(
                     json_poll_questions,
-                    CONCAT('$[', len_json_poll_questions-1, '].options')
+                    CONCAT('$[', count_questions, '].options')
                 );
 
             set len_json_poll_questions_options = JSON_LENGTH(json_poll_questions_options);
 
-            while len_json_poll_questions_options > 0 do
+            while count_questions_options < len_json_poll_questions_options do
                     INSERT encuestas_optionsquestion(
                         name_option_question,
                         value_option_question,
@@ -78,19 +80,19 @@ BEGIN
                                  JSON_VALUE(
                                          JSON_EXTRACT(
                                                  json_poll_questions_options,
-                                                 CONCAT('$[', len_json_poll_questions_options - 1, ']')),
+                                                 CONCAT('$[', count_questions_options, ']')),
                                          '$.name_option'
                                      ),
                                  JSON_VALUE(
                                          JSON_EXTRACT(
                                                  json_poll_questions_options,
-                                                 CONCAT('$[', len_json_poll_questions_options - 1, ']')),
+                                                 CONCAT('$[', count_questions_options, ']')),
                                          '$.value_option'
                                      ),
                                  JSON_VALUE(
                                          JSON_EXTRACT(
                                                  json_poll_questions_options,
-                                                 CONCAT('$[', len_json_poll_questions_options - 1, ']')),
+                                                 CONCAT('$[', count_questions_options, ']')),
                                          '$.title_option'
                                      ),
                                  '',
@@ -98,15 +100,16 @@ BEGIN
                                  JSON_VALUE(
                                          JSON_EXTRACT(
                                                  json_poll_questions_options,
-                                                 CONCAT('$[', len_json_poll_questions_options - 1, ']')),
+                                                 CONCAT('$[', count_questions_options, ']')),
                                          '$.id_question'
                                      )
                              );
 
-                    set len_json_poll_questions_options = len_json_poll_questions_options - 1;
+                    set count_questions_options = count_questions_options + 1;
                 end while;
+            set count_questions_options = 0;
 
-            set len_json_poll_questions = len_json_poll_questions - 1;
+            set count_questions = count_questions + 1;
         end while;
 end//
 DELIMITER ;
