4df369194a42c24eeeed3fd91f862009a7989310
JS Martinez <jason.guerra253@gmail.com>
2023-06-03 00:41:44 -0600

    Fixed route type questions

--- encuestas/url.py
+++ encuestas/url.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .Controller.PollController import create_poll, store_poll, get_poll, get_data_poll
+from .Controller.PollController import create_poll, store_poll, get_poll, get_data_poll, response_poll, get_type_question
+from .Controller.HomeController import home
 
 from . import views
 
@@ -9,7 +10,11 @@ urlpatterns = [
     path("create-poll", create_poll, name="create_poll"),
     path("save-poll", store_poll, name="save_poll"),
     path("get-poll/<str:token_poll>", get_poll, name="get_poll"),
-    path("get-data-poll/<str:token_poll>", get_data_poll, name="get_data_poll"),
+    path("get-type-questions", get_type_question, name="get_type_question"),
 
+    path("response-poll/<str:token_poll>", response_poll, name="response_poll"),
+
+    path("get-data-poll/<str:token_poll>", get_data_poll, name="get_data_poll"),
+    path("home", home, name="home")
 
 ]
